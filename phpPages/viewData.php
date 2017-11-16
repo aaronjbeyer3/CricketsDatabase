e661d832bb4c02ecb6d79ff4542e91e3487c1495
Aaron Beyer <aaronjbeyer3@hotmail.com>
2017-11-16 10:44:42 -0600

    Added php page for viewing rows in Tables.

---
+++ phpPages/viewData.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>
+
+<h1>Crickets Database: View Data</h1>
+<p>(Select the table you want to view data from)</p><br>
+
+<?php
+//define variables
+$nameErr = "";
+$name = "";
+$valid = 0;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["tname"]))
+    $nameErr = "Table is required";
+  else
+  {
+    $name = $_POST["tname"];
+
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+    else
+    	$valid = 1;
+  }
+}
+?>
+
+<p><span class="error">* required field.</span></p>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+  <p>Current Tables: Cricket, TestInstance, Test, Project, Observer, CricketTestInstance</p><br><br>
+  Table Name: 
+  <select name="tname">
+    <option value="">Select Table</option>
+    <option value="TestInstance">TestInstance Table</option>
+    <option value="Cricket">Cricket Table</option>
+    <option value="Test">Test Table</option>
+    <option value="Project">Project Table</option>
+    <option value="Observer">Observer Table</option>
+    <option value="CricketTestInstance">CricketTestInstance Table</option>
+  </select>
+  <span class="error">* <?php echo $nameErr;?></span><br><br>
+  <input type="submit">
+</form><br><br>
+
+<h1>Data From <?php echo $name;?> Table:</h1>
+
+<?php
+// Only query the database if the name is valid
+if($valid == 1)
+{
+	// Connection string
+	$conn = oci_connect('aabeyer', 'Piggies3!', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+
+	$query = "SELECT * FROM " . $name;
+	$stid = oci_parse($conn,$query);
+	oci_execute($stid,OCI_DEFAULT);
+
+	// If there are no records, say so.
+	if(oci_fetch_all($stid, $res) < 1)
+	{
+		echo "No results for '" . $name ."'";
+		echo '<br/>';
+	}
+
+	// Execute the query a second time because I used the fetch_all function above
+	oci_execute($stid,OCI_DEFAULT);
+
+	//iterate through each row
+	while ($row = oci_fetch_array($stid,OCI_ASSOC))
+	{
+		//iterate through each item in the row and echo it
+		foreach ($row as $item)
+		{
+			echo $item.' ';
+		}
+
+		echo '<br/>';
+	}
+	oci_free_statement($stid);
+	oci_close($conn);
+}
+?>
+
+</body>
+</html>
