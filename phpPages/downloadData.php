d0b52c25f374869da5d1ce2fa90203dc751fb335
Aaron Beyer <aaronjbeyer3@hotmail.com>
2017-11-19 20:54:14 -0600

    Added functionality for "All Data" or "Filtered Data"

--- phpPages/downloadData.php
+++ phpPages/downloadData.php
@@ -20,70 +20,70 @@
     });
 </script>
 
-<h1>Crickets Database: Download Data</h1>
-<p>(currently all data in the database joined together)</p><br>
+<h1>Crickets Database: Download Data</h1><br>
+<h2>Filter Data</h2>
 
 <?php
-//define variables
-$nameErr = "";
-$name = "";
+// Define variables
+$name = "All";
 $cValue = "";
 $cValueErr = "";
-$valid = 0;
+$valid = 1;
+$ResultsType = "All Data:";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["tname"]))
-    $nameErr = "Criteria is required";
-  else
+  $name = $_POST["tname"];
+
+  if (empty($_POST["cName"]) && $_POST["tname"] != "All")
   {
-    $name = $_POST["tname"];
-    $valid++;
-  }
-  if (empty($_POST["cName"]))
     $cValueErr = "Value is required";
+    $valid = 0;
+  }   
   else
   {
     $cValue = $_POST["cName"];
 
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z0-9 ]*$/",$cValue)) {
+    // Check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z0-9 ]*$/",$cValue))
+    {
         $cValueErr = "Only letters and white space allowed";
+        $valid = 0;
     }
-    else
-        $valid++;
   }
+
+  // Set Table Label
+  if ($name != "All")
+    $ResultsType = "Filtered Data:";
 }
 ?>
 
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
   Criteria: 
   <select name="tname">
-    <option value="">Select Criteria</option>
     <option value="All">All Data</option>
     <option value="t.tname">Test Name</option>
     <option value="c.ID">Cricket ID</option>
     <option value="o.oName">Observer</option>
     <option value="ti.arena">Arena</option>
-  </select>
-  <span class="error"><?php echo $nameErr;?></span> = 
+  </select> =
   <input type="text" name="cName">
   <span class="error"><?php echo $cValueErr;?></span><br><br>
   <input type="submit">
 </form><br><br>
 
-<h1>Results:</h1>
+<h2><?php echo $ResultsType ?></h2>
 
 <table style='border: 1px solid black' id = "export_table">
 <?php
-if($valid == 2)
+if($valid == 1)
 {
     // Connection string
     $conn = oci_connect('aabeyer', 'Piggies3!', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
 
     $criteria = "";
-    if($name == "c.ID" OR $name == "ti.arena")
+    if ($name == "c.ID" OR $name == "ti.arena")
         $criteria = "WHERE " . $name . " = " . $cValue;
-    else
+    else if ($name != "All")
         $criteria = "WHERE " . $name . " = '" . $cValue . "'";
 
     $query = "SELECT c.name AS Id, t.tName AS Test, c.mother AS Mom, " .
@@ -111,7 +111,7 @@ if($valid == 2)
     //---Start of data table---
     $out = '';
 
-    //add column headers to table
+    // Add column headers to table
     $fieldIndex = 1;
     $out .= "<tr style='border: 1px solid black'>";
     while ($row = oci_field_name($stid,$fieldIndex))
@@ -121,26 +121,31 @@ if($valid == 2)
     }
     $out .= "</tr>";
 
-    //iterate through each row
+    // Iterate through each entry in results
     while ($row = oci_fetch_array($stid,OCI_RETURN_NULLS + OCI_ASSOC))
     {
+        // Start next row in table
         $out .= "<tr style='border: 1px solid black'>";
-        //iterate through each item in the row and echo it
+
+        // Fill each column in the row
         foreach ($row as $item)
         {
-            //echo $item.' ';
             $out .= "<td style='border: 1px solid black; padding: 5px'>".$item."</td>"; 
         }
+
+        // End row
         $out .= "</tr>";
     }
     oci_free_statement($stid);
     oci_close($conn);
 
-    echo ($out); 
+    //Print out table to page
+    echo ($out);
+    //---End of data table---
 } 
 ?>
 </table><br/>
-
+<!-- USED TO DEBUG QUERY <p><?php echo $query ?></p> -->
 <button id="export" data-export="export">Export As CSV</button>
 
 </body>
