8c328b1d2adecbe813dc93df787f446e7230bff4
Aaron Beyer <aaronjbeyer3@hotmail.com>
2017-11-21 21:43:50 -0600

    More progress on file upload. Cricket table insert done.

--- phpPages/uploadData.php
+++ phpPages/uploadData.php
@@ -26,154 +26,135 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js" type="text/javascript" charset="utf-8"></script>
 <script src="./jquery.tabletoCSV.js" type="text/javascript" charset="utf-8"></script>
 
-<h2>Filter Data (Optional)</h2>
+<h2>Test</h2>
 
 <?php
-// Define variables
-$name = "All";
-$cValue = "";
-$cValueErr = "";
-$valid = 1;
-$ResultsType = "All Data (Default):";
-$fileCaption = "Crickets-ALL-Data";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST")
+if(!empty($_POST))
 {
-  $name = $_POST["tname"];
-
-  if (empty($_POST["cName"]) && $_POST["tname"] != "All")
-  {
-    $cValueErr = "Value is required";
-    $valid = 0;
-  }   
-  else
-  {
-    $cValue = $_POST["cName"];
-
-    // Check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z0-9 ]*$/",$cValue))
+    if ($_FILES["file"]["error"] > 0)
     {
-        $cValueErr = "Only letters and white space allowed";
-        $valid = 0;
+        echo "Error: " . $_FILES["file"]["error"] . "<br />";
     }
-  }
+    else
+    {
+        // Set connection string
+        $conn = oci_connect('aabeyer', 'Piggies3!', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
 
-  // Set Table Label and File Caption
-  if ($name != "All")
-  {
-    $ResultsType = "Filtered Data:";
+        // Declare variables
+        $f = file($_FILES["file"]["tmp_name"]);
+        $firstLine = true;
+        $testCounter = 0;
 
-    // File Caption 
-    if($name == "t.tname")
-        $fileCaption = "Crickets-FILTERED(Test)-Data";
-    else if ($name == "c.ID")
-        $fileCaption = "Crickets-FILTERED(CricketID)-Data";
-    else if ($name == "o.oName")
-        $fileCaption = "Crickets-FILTERED(Observer)-Data";
-    else if ($name == "ti.arena")
-        $fileCaption = "Crickets-FILTERED(Arena)-Data";
-  }   
+        // Variables for inserts
+        $cricketID = "";
+        $projectID = "";
+        $testID = "";
+        $observerID = "";
+
+        //loop through each line
+        foreach($f as $key => $value)
+        { 
+            if($firstLine)
+            {
+                // cut the whitespaces from the beginning and end
+                $value = trim($value);
+                $array = explode(",", $value);
+                
+                // trim the values for the database
+                array_walk($array, trim);
+                $query = "Fields that will be inserted into the databse: $array[2], $array[3], $array[4], $array[5], $array[6], $array[7], $array[8], $array[9], $array[10]";
+                echo "$query<br /><br />";
+
+                $firstLine = false;
+            }
+            else // Loop through each record
+            {
+                if($testCounter < 1)
+                {
+                    // cut the whitespaces from the beginning and end
+                    $value = trim($value);
+                    $array = explode(",", $value);
+                    array_walk($array, trim); // trim the values for the database
+
+                    //WRITE QUERIES
+                    echo "============================================ NEW RECORD ============================================<br />";
+
+                    
+                    // Cricket Table ===============================================================================================
+                    echo "Cricket Table:<br />";
+
+                    // Determine CricketID
+                    $query = "SELECT * FROM Cricket WHERE name = '$array[2]'";
+                    $stid = oci_parse($conn,$query);
+
+                    oci_execute($stid,OCI_DEFAULT);
+                    //$row = oci_fetch_array($stid,OCI_RETURN_NULLS + OCI_ASSOC);
+                    oci_fetch($stid);
+                    $cricketID = oci_result($stid, 'ID');
+
+                    //if the Cricket is not already in the database
+                    if($cricketID == "")
+                    {
+                        $query = "SELECT MAX(ID) AS ID FROM Cricket";
+                        echo "$query<br />";
+                        $stid = oci_parse($conn,$query);
+                        oci_execute($stid,OCI_DEFAULT);
+                        oci_fetch($stid);
+                        $cricketID = "" . ((int)(oci_result($stid, 'ID')) + 1);
+
+                        // Add new Cricket to Database
+                        $query = "INSERT INTO Cricket VALUES" .
+                                " ($cricketID, '$array[2]'," .                          
+                                " CAST(SUBSTR('$array[2]', INSTR('$array[2]', '_', 1, 2) + 1, INSTR('$array[2]', '_', 1, 3) - INSTR('$array[2]', '_', 1, 2) - 1) AS INTEGER)," .
+                                " CAST(SUBSTR('$array[2]', INSTR('$array[2]', '_', 1, 3) + 1, INSTR('$array[2]', '_', 1, 4) - INSTR('$array[2]', '_', 1, 3) - 1) AS INTEGER))";
+                        echo "$query<br />";
+                        $stid = oci_parse($conn,$query);
+                        oci_execute($stid,OCI_COMMIT_ON_SUCCESS);
+                        echo "query executed<br />";
+                    }
+                    else
+                        echo "Cricket already exists<br />";                    
+                    echo "Cricket ID from database: $cricketID<br />";
+
+
+                    // Observer Table ===============================================================================================
+                    echo "<br />Observer Table:<br />";
+                    $query = "INSERT INTO Observer values (GET-ID-FROM('$array[9]'), '$array[9]')";
+                    echo "$query<br />";
+
+                    // Project Table ===============================================================================================
+                    echo "<br />Project Table:<br />";
+                    $query = "INSERT INTO Project values (GET-ID-FROM('InputtedProject'), 'InputtedProject')";
+                    echo "$query<br />";
+
+                    // Test Table ===============================================================================================
+                    echo "<br />Test Table:<br />";
+                    $query = "INSERT INTO Test values (GET-ID-FROM('InputtedTest'), 'InputtedTest')";
+                    echo "$query<br />";
+
+                    // TestInstance Table ===============================================================================================
+                    echo "<br />TestInstance Table:<br />";
+                    $query = "INSERT INTO TestInstance values (GET-ID-FROM(MaxValueInTheTable + 1), TestIDValueFromAbove, '$array[3]', '$array[4]', '$array[5]', '$array[6]', '$array[7]', TO_DATE('$array[8]', 'MM/DD/YYYY'), ObserverIDValueFromAbove, '$array[10]')";
+                    echo "$query<br />";
+
+                    // CricketTestInstanceTable ===============================================================================================
+                    echo "<br />CricketTestInstance Table:<br />";
+                    $query = "INSERT INTO CricketTestInstance values (GET-ID-FROM(MaxValueInTheTable + 1), CricketIDFromAbove, TestInstanceIDFromAbove)";
+                    echo "$query<br />";
+
+                    $testCounter++;
+                }
+            }
+        }
+    }    
 }
 ?>
 
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-  Criteria: 
-  <select name="tname">
-    <option value="All">All Data</option>
-    <option value="t.tname">Test Name</option>
-    <option value="c.ID">Cricket ID</option>
-    <option value="o.oName">Observer</option>
-    <option value="ti.arena">Arena</option>
-  </select> =
-  <input type="text" name="cName">
-  <span class="error"><?php echo $cValueErr;?></span><br><br>
-  <input type="submit">
-</form><br><br>
-
-<h2><?php echo $ResultsType ?></h2>
-
-<table style='border: 1px solid black' id = "export_table">
-<?php
-if($valid == 1)
-{
-    // Connection string
-    $conn = oci_connect('aabeyer', 'Piggies3!', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
-
-    $criteria = "";
-    if ($name == "c.ID" OR $name == "ti.arena")
-        $criteria = "WHERE " . $name . " = " . $cValue;
-    else if ($name != "All")
-        $criteria = "WHERE " . $name . " = '" . $cValue . "'";
-
-    $query = "SELECT c.name AS Id, t.tName AS Test, c.mother AS Mom, " .
-    "c.father AS Dad, ti.recordingTime AS testTime, ti.mass, ti.rep, " .
-    "ti.arena, ti.temp, ti.recordingDate AS testDate, o.oName AS Observer, ti.status " .
-    "FROM TestInstance ti " .
-    "LEFT JOIN CricketTestInstance cti ON ti.ID = cti.testInstanceID " .
-    "INNER JOIN Cricket c ON cti.cricketID = c.ID " .
-    "LEFT JOIN Test t ON ti.testID = t.ID " .
-    "INNER JOIN Project p ON t.projectID = p.ID " .
-    "LEFT JOIN Observer o ON ti.observerID = o.ID " . $criteria;
-
-    $stid = oci_parse($conn,$query);
-    oci_execute($stid,OCI_DEFAULT);
-
-    // If there are no records, say so.
-    if(oci_fetch_all($stid, $res) < 1)
-    {
-        echo "<tr><td>". "No results for '" . $name ."'</td></tr>";
-    }
-
-    // Execute the query a second time because I used the fetch_all function above
-    oci_execute($stid,OCI_DEFAULT);
-
-    //---Start of data table---
-    $out = '';
-
-    // Add column headers to table
-    $fieldIndex = 1;
-    $out .= "<tr>";
-    while ($row = oci_field_name($stid,$fieldIndex))
-    {
-        $out .= "<td>". oci_field_name($stid,$fieldIndex) ."</td>"; 
-        $fieldIndex++;
-    }
-    $out .= "</tr>";
-
-    // Iterate through each entry in results
-    while ($row = oci_fetch_array($stid,OCI_RETURN_NULLS + OCI_ASSOC))
-    {
-        // Start next row in table
-        $out .= "<tr>";
-
-        // Fill each column in the row
-        foreach ($row as $item)
-        {
-            $out .= "<td>".$item."</td>"; 
-        }
-
-        // End row
-        $out .= "</tr>";
-    }
-    oci_free_statement($stid);
-    oci_close($conn);
-
-    //Print out table to page
-    echo ($out);
-    //---End of data table---
-} 
-?>
-</table><br/>
-<!-- USED TO DEBUG QUERY <p><?php echo $query ?></p> -->
-
-<script>
-    $(function(){
-        $("#export").click(function(){
-            $("#export_table").tableToCSV("<?php echo $fileCaption ?>");
-        });
-    });
-</script>
-<button id="export" data-export="export">Export As CSV</button>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
+<label for="file">Filename:</label>
+<input type="file" name="file" id="file" /> 
+<input type="submit" name="submit" value="Submit" />
+</form>
 
 </body>
 </html>
