7d338b6ae8be1350553868c66f194842112f3bcc
Aaron Beyer <aaronjbeyer3@hotmail.com>
2017-11-21 21:42:48 -0600

    reformatted download query

--- phpPages/downloadData.php
+++ phpPages/downloadData.php
@@ -105,15 +105,15 @@ if($valid == 1)
     else if ($name != "All")
         $criteria = "WHERE " . $name . " = '" . $cValue . "'";
 
-    $query = "SELECT c.name AS Id, t.tName AS Test, c.mother AS Mom, " .
-    "c.father AS Dad, ti.recordingTime AS testTime, ti.mass, ti.rep, " .
-    "ti.arena, ti.temp, ti.recordingDate AS testDate, o.oName AS Observer, ti.status " .
-    "FROM TestInstance ti " .
-    "LEFT JOIN CricketTestInstance cti ON ti.ID = cti.testInstanceID " .
-    "INNER JOIN Cricket c ON cti.cricketID = c.ID " .
-    "LEFT JOIN Test t ON ti.testID = t.ID " .
-    "INNER JOIN Project p ON t.projectID = p.ID " .
-    "LEFT JOIN Observer o ON ti.observerID = o.ID " . $criteria;
+    $query = "SELECT c.name AS Id, t.tName AS Test, c.mother AS Mom," .
+            " c.father AS Dad, ti.recordingTime AS testTime, ti.mass, ti.rep," .
+            " ti.arena, ti.temp, ti.recordingDate AS testDate, o.oName AS Observer, ti.status" .
+            " FROM TestInstance ti" .
+            " LEFT JOIN CricketTestInstance cti ON ti.ID = cti.testInstanceID" .
+            " INNER JOIN Cricket c ON cti.cricketID = c.ID" .
+            " LEFT JOIN Test t ON ti.testID = t.ID" .
+            " INNER JOIN Project p ON t.projectID = p.ID" .
+            " LEFT JOIN Observer o ON ti.observerID = o.ID " . $criteria;
 
     $stid = oci_parse($conn,$query);
     oci_execute($stid,OCI_DEFAULT);
